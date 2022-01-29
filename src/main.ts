b0d461dc2899aa128e98824e68bb3c8410c78b66
Yaraslau Tabolich <yaraslau_tabolich@epam.com>
2022-01-29 18:43:27 +0300

    feat: add implementation for a lazy plugin

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from "@/router";
-import lazyLoader from '@/directives/lazyLoader';
 import Formatter from "@/utils/Formatter";
+import lazy, {IOptionsLazy} from "@/plugins/lazy";
 
 
 const app = createApp(App);
 
 app
   .use(router)
+  .use(lazy, {
+    height: 592,
+    width: 395,
+    placeText: 'Loading...'
+  } as IOptionsLazy)
   .provide('formatter', Formatter)
-  .directive('lazy', lazyLoader)
   .mount('#app');
